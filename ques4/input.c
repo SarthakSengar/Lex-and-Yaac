0a492c59ab9eb209b45fedfe64ae769deb62f47d
SarthakSengar <sarthak.sengar11366@gmail.com>
2023-08-26 16:08:00 +0530

    lex progs

---
+++ ques4/input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    //comment
+  float f = 1.23 ;
+  int i = 1 ;
+  float j = f + i;
+  return 0;
+}
---
+++ ques6/prog.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+  float f_id = 1.23 ;
+  int i_id = 1 ;
+  char s_id = 's' ;
+  return 0;
+}
